33f06ad5caadeb3e883a0a1d2296b7dd48086eca
Papai <papairoy080@gmail.com>
2023-08-29 15:55:36 +0530

    project id and privatekey updated

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.post("/authenticate", async (req, res) => {
     const r = await axios.put(
       "https://api.chatengine.io/users/",
       { username: username, secret: username, first_name: username },
-      { headers: { "private-key": "e3c377da-d186-4014-891a-a3c94fe34639" } }
+      { headers: { "private-key": "685699e6-01dd-402b-bf3d-647251ae57b0" } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
     return (
     <div style={{height: '100vh'}}>
         <PrettyChatWindow
-        projectId='95ec8697-56dd-42b0-84dd-7f5af65f4500'
+        projectId='bd9d88e3-39f5-4726-af22-ce33007bf138'
         username={props.user.username}
         secret={props.user.secret}
         style={{height: '100%'}}
